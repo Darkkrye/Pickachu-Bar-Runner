157341987d0ae693ef27bd6fbd39f967ac35b4b7
Darkkrye <pierre.boudon.27@gmail.com>
2016-12-22 20:57:03 +0100

    Initial Commit

---
+++ Pikachu-Bar-Runner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pikachu-Bar-Runner
+//
+//  Created by Pierre on 22/12/2016.
+//  Copyright © 2016 Pierre BOUDON. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pikachu-Bar-Runner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pikachu-Bar-Runner
+//
+//  Created by Pierre on 22/12/2016.
+//  Copyright © 2016 Pierre BOUDON. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
